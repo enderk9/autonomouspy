e9781ce5e2d5a8c54436a81a1de574b8ba5e47d2
enderk9 <106938714+enderk9@users.noreply.github.com>
2024-11-22 12:51:21 +0700

    main.py

---
+++ main.py
@@ -0,0 +1,305 @@
+import threading,base64
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+import os,time,re,json,random
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+from datetime import datetime, timedelta
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+from time import sleep,strftime
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+from bs4 import BeautifulSoup
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+import requests
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+import os, sys
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+import socket
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+try:
+  ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+  from faker import Faker
+  ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+  from requests import session
+  ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+  from colorama import Fore, Style
+  ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+  import requests, random, re
+  ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+  from random import randint
+  ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+  import requests,pystyle
+  ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+  import socks
+  ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+except:
+  ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+  ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+  os.system("pip install faker")
+  ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+  os.system("pip install requests")
+  ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+  os.system("pip install colorama")
+  ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+  os.system('pip install requests && pip install bs4 && pip install pystyle')
+  ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+  os.system("pip3 install requests pysocks")
+  ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+  print('__Vui Lòng Chạy Lại Tool__')
+  ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+xnhac = "\033[1;36m"
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+do = "\033[1;31m"
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+luc = "\033[1;32m"
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+vang = "\033[1;33m"
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+xduong = "\033[1;34m"
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+hong = "\033[1;35m"
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+trang = "\033[1;39m"
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+whiteb="\033[1;39m"
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+red="\033[0;31m"
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+redb="\033[1;31m"
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+end='\033[0m'
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+dev="\033[1;39m[\033[1;31m×\033[1;39m]\033[1;39m"
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+def banner():
+ ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+ banner = f"""
+\033[1;33m██      ██╗      ████████╗ █████╗  █████╗ ██╗
+\033[1;35m██╗    ╔██║      ╚══██╔══╝██╔══██╗██╔══██╗██║
+\033[1;36m██║████║██║ █████╗  ██║   ██║  ██║██║  ██║██║  - cracked
+\033[1;37m██║    ╚██║ ╚════╝  ██║   ██║  ██║██║  ██║██║
+\033[1;32m██║     ██║         ██║   ╚█████╔╝╚█████╔╝██████╗
+\033[1;31m╚═╝     ╚═╝         ╚═╝    ╚════╝  ╚════╝ ╚═════╝\n
+\033[1;97mTool By: \033[1;32mTrịnh Hướng            \033[1;97mPhiên Bản: \033[1;32m4.0 
+\033[1;97mCracked By: \033[1;32mhngl - Le Viet Hung :>           \033[1;97mPhiên Bản: \033[1;32m4.0       
+\033[97m════════════════════════════════════════════════  
+\033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Youtube\033[1;31m  : \033[1;97m☞ \033[1;36mHướng Dev - Kiếm Tiền Online\033[1;31m♔ \033[1;97m☜
+\033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Tik Tok\033[1;31m  : \033[1;33mhttps:\033[1;32m//www.tiktok.com\033[1;31m/m@huongdev27
+\033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Zalo\033[1;31m     : \033[1;97m☞\033[1;31m0\033[1;37m3\033[1;36m6\033[1;35m2\033[1;34m1\033[1;33m6\033[1;33m6\033[1;34m8\033[1;35m6\033[1;37m3☜
+\033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Facebook\033[1;31m : \033[1;97mi.urs.bin.python.TrinhHuong 
+\033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Telegram\033[1;31m : \033[1;97m☞\033[1;32mhttps://t.me/+77MuosyD-yk4MGY1🔫\033[1;97m☜
+\033[97m════════════════════════════════════════════════
+"""
+ ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+ for X in banner:
+  ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+  sys.stdout.write(X)
+  ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+  sys.stdout.flush() 
+  ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+  sleep(0.000001)
+  ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+def bes4(url):
+	############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+    # Gửi yêu cầu GET đến URL
+	############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+    response = requests.get(url)
+	############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+    
+    ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+    # Nếu yêu cầu thành công (status code 200)
+	############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+    if response.status_code == 200:
+		############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+        # Phân tích nội dung HTML của trang web
+		############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+        soup = BeautifulSoup(response.content, 'html.parser')
+		############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+        
+        ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+        # Tìm thẻ <span> chứa thông tin phiên bản và trạng thái bảo trì
+		############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+        version_tag = soup.find('span', id='version0')
+		############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+		############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+        maintenance_tag = soup.find('span', id='maintenance0')
+		############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+        
+        ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+		############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+        # Lấy nội dung văn bản bên trong thẻ
+		############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+        version = version_tag.text.strip() if version_tag else None
+		############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+        maintenance = maintenance_tag.text.strip() if maintenance_tag else None
+		############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+        
+        ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+        return version, maintenance
+	############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+    
+    ############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+    return None, None
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################
+
+current_version = "1.2"
+if current_version:
+############################### CRACKED BY LEVIETHUNG - HNGL ###############################  
+    print(f"Phiên bản hiện tại: {current_version}")
+else:
+    print("Không thể lấy thông tin phiên bản hoặc tool đang được bảo trì.")
+# Hàm để lấy địa chỉ IP của thiết bị
+def get_ip_address():
+    try:
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
+        return ip_address
+    except:
+        return None
+
+# Hàm để hiển thị địa chỉ IP của thiết bị
+def display_ip_address(ip_address):
+    if ip_address:
+        banner = """
+\033[1;33m██      ██╗      ████████╗ █████╗  █████╗ ██╗
+\033[1;35m██╗    ╔██║      ╚══██╔══╝██╔══██╗██╔══██╗██║
+\033[1;36m██║████║██║ █████╗  ██║   ██║  ██║██║  ██║██║
+\033[1;37m██║    ╚██║ ╚════╝  ██║   ██║  ██║██║  ██║██║
+\033[1;32m██║     ██║         ██║   ╚█████╔╝╚█████╔╝██████╗
+\033[1;31m╚═╝     ╚═╝         ╚═╝    ╚════╝  ╚════╝ ╚═════╝\n
+\033[1;97mTool By: \033[1;32mTrịnh Hướng            \033[1;97mPhiên Bản: \033[1;32m4.0   
+\033[1;97mCracked By: \033[1;32mhngl            \033[1;97mPhiên Bản: \033[1;32m4.0     
+\033[97m════════════════════════════════════════════════  
+\033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Tool   \033[1;31m  : \033[1;97m☞ \033[1;31mTool Gộp Vip♔ \033[1;97m☜
+\033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Tik Tok\033[1;31m  : \033[1;33mhttps:\033[1;32m//www.tiktok.com\033[1;31m/m@huongdev27
+\033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Zalo\033[1;31m     : \033[1;97m☞\033[1;31m0\033[1;37m3\033[1;36m6\033[1;35m2\033[1;34m1\033[1;33m6\033[1;33m6\033[1;34m8\033[1;35m6\033[1;37m3☜
+\033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Facebook\033[1;31m : \033[1;97mi.urs.bin.python.TrinhHuong 
+\033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Telegram\033[1;31m : \033[1;97m☞\033[1;32mhttps://t.me/+77MuosyD-yk4MGY1🔫\033[1;97m☜
+\033[97m════════════════════════════════════════════════
+\033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Youtube\033[1;31m  : \033[1;97m☞ \033[1;36mHướng Dev - Kiếm Tiền Online\033[1;31m♔ \033[1;97m☜
+"""
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+        for x in banner:
+            print(x, end="")
+            time.sleep(0.001)
+
+        print(f"\033[1;32mĐịa chỉ IP : {ip_address}     Version: {current_version}")
+    else:
+        print("Không thể lấy địa chỉ IP của thiết bị.")
+
+
+while True:
+	os.system('cls' if os.name == 'nt' else 'clear')
+	banner()
+	print("\033[1;37m╔══════════════════════╗         ")
+	print("\033[1;37m║  \033[1;32mTool Auto Golike    \033[1;37m║          ")
+	print("\033[1;37m╚══════════════════════╝           ")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m1 \033[1;97m: \033[1;34mTool Auto TikTok \033[1;32m[Online] \033[1;32m[Termux]")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m2 \033[1;97m: \033[1;34mTool Auto TikTok Tự Động \033[1;32m[Online] \033[1;32m[Termux + PC]")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m3 \033[1;97m: \033[1;34mTool Auto Instagram \033[1;32m[Online] \033[1;32m[Termux + PC]")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m4 \033[1;97m: \033[1;34mTool Auto Twitter \033[1;32m[Online] \033[1;32m[Termux + PC]")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m5 \033[1;97m: \033[1;34mTool Auto Youtube \033[1;32m[Online] \033[1;32m[Termux + PC]")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m6 \033[1;97m: \033[1;34mTool Auto Thread \033[1;32m[Online] \033[1;32m[Termux + PC]")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m7 \033[1;97m: \033[1;34mTool Auto Linkedin \033[1;32m[Online] \033[1;32m[Termux + PC]")
+	print("\033[1;37m╔══════════════════════╗         ")
+	print("\033[1;37m║  \033[1;32mTool Tương Tác Chéo \033[1;37m║          ")
+	print("\033[1;37m╚══════════════════════╝           ")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m8 \033[1;97m: \033[1;34mTool TTC Facebook \033[1;32m[Online] \033[1;32m[Termux + PC]")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m9 \033[1;97m: \033[1;34mTool TTC Pro5 \033[1;32m[Online] \033[1;32m[Termux + PC]")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m10 \033[1;97m: \033[1;34mTool TTC Pro5v1 \033[1;32m[Online] \033[1;32m[Termux + PC]")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m11 \033[1;97m: \033[1;34mTool TTC TikTok \033[1;32m[Online] \033[1;32m[Termux + PC]")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m12 \033[1;97m: \033[1;34mTool TTC Instagram \033[1;32m[Online] \033[1;32m[Termux + PC]")
+	print("\033[1;37m╔══════════════════════╗         ")
+	print("\033[1;37m║  \033[1;32mTool TraoDoiSub.com \033[1;37m║          ")
+	print("\033[1;37m╚══════════════════════╝           ")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m13 \033[1;97m: \033[1;34mTool TDS Facebook \033[1;32m[Online] \033[1;32m[Termux + PC]")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m14 \033[1;97m: \033[1;34mTool TDS Pro5 \033[1;32m[Online] \033[1;32m[Termux + PC]")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m15 \033[1;97m: \033[1;34mTool TDS Pro5v1 \033[1;32m[Online] \033[1;32m[Termux + PC]")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m16 \033[1;97m: \033[1;34mTool TDS TikTok \033[1;32m[Online] \033[1;32m[Termux + PC]")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m17 \033[1;97m: \033[1;34mTool TDS Instagram \033[1;32m[Online] \033[1;32m[Termux + PC]")
+	print("\033[1;37m╔══════════════════════╗         ")
+	print("\033[1;37m║  \033[1;32mTool Tiện Ích \033[1;37m      ║   ")
+	print("\033[1;37m╚══════════════════════╝           ")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m18 \033[1;97m: \033[1;34mTool Get ID Facebook \033[1;32m[Online]")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m19 \033[1;97m: \033[1;34mTool Get Token Facebook \033[1;32m[Online]")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m20 \033[1;97m: \033[1;34mTool Spam Message \033[1;32m[Online]")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m21 \033[1;97m: \033[1;34mTool Share Ảo Facebook \033[1;32m[Online]")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m22 \033[1;97m: \033[1;34mTool Đào Mail \033[1;32m[Online]")
+	print(f"\033[1;97m[\033[1;32m*\033[1;97m] \033[1;33m23 \033[1;97m: \033[1;34mThoát Tool \033[1;32m[Online]")
+	print(f"\033[97m════════════════════════════════════════════════════════")
+	chon = input('\033[1;91m┌─╼\033[1;97m[\033[1;91m<\033[1;97m/\033[1;91m>\033[1;97m]--\033[1;91m>\033[1;97m Nhập lựa chọn \033[1;97m \n\033[1;91m└─╼\033[1;91m✈ \033[1;33m : ')
+	print('\033[1;39m─────────────────────────────────────────────────────────── ')
+	if chon == '1':
+		# Thành Công
+		exec(open('tools/Full Golike/AutoTikTokv1.py', 'r', encoding='utf-8').read())
+	elif chon == '2':
+		exec(open('tools/Full Golike/AutoTikTokv2.py', 'r', encoding='utf-8').read())
+	elif chon == '3':
+		exec(open('tools/Full Golike/AutoIG.py', 'r', encoding='utf-8').read())
+	elif chon == '4':
+		exec(open('tools/Full Golike/AutoX.py', 'r', encoding='utf-8').read())
+	elif chon == '5':
+		exec(open('tools/Full Golike/AutoYTB.py', 'r', encoding='utf-8').read())
+	elif chon == '6':
+		exec(open('tools/Full Golike/AutoTheads.py', 'r', encoding='utf-8').read())
+	elif chon == '7':
+		exec(open('tools/Full Golike/AutoLinkedin.py', 'r', encoding='utf-8').read())
+		# TTC
+	elif chon == '8':
+		exec(open('tools/TuongTacCheo/TTCFB.py', 'r', encoding='utf-8').read())
+	elif chon == '9':
+		exec(open('tools/TuongTacCheo/TTCPro5.py', 'r', encoding='utf-8').read())
+	elif chon == '10':
+		exec(open('tools/TuongTacCheo/TTCPro5v1.py', 'r', encoding='utf-8').read())
+	elif chon == '11':
+		exec(open('tools/TuongTacCheo/TTCTikTok.py', 'r', encoding='utf-8').read())
+	elif chon == '12':
+		exec(open('tools/TuongTacCheo/TTCIG.py', 'r', encoding='utf-8').read())
+		# TDS
+	elif chon == '13':
+		# Thanh Công
+		exec(open('tools/TraoDoiSub/ToolTDSFullJob.py', 'r', encoding='utf-8').read())
+	elif chon == '14':
+		# Thanh Công
+		exec(open('tools/TraoDoiSub/ToolTDSPro5.py', 'r', encoding='utf-8').read())
+	elif chon == '15':
+		# Thanh Công
+		exec(open('tools/TraoDoiSub/ToolTDSPro5v1.py', 'r', encoding='utf-8').read())
+	elif chon == '16':
+		# Thanh Công
+		exec(open('tools/TraoDoiSub/ToolTDSTikTok.py', 'r', encoding='utf-8').read())
+	elif chon == '17':
+		# Thanh Công
+		exec(open('tools/TraoDoiSub/TDSIG.py', 'r', encoding='utf-8').read())	
+		# Tiên ích
+	elif chon == '18':
+		# Thanh Công
+		exec(open('tools/TienIchFaceBook/ToolGetidFacebook.py', 'r', encoding='utf-8').read())
+	elif chon == '19':
+		# Thanh Công
+		exec(open('tools/TienIchFaceBook/ToolGetTokenFB.py', 'r', encoding='utf-8').read())
+	elif chon == '20':
+		# Thanh Công
+		exec(open('tools/TienIchFaceBook/ToolSpamMessage.py', 'r', encoding='utf-8').read())
+	elif chon == '21':
+		# Thanh Công
+		exec(open('tools/TienIchFaceBook/ToolShareAoCookieV1.py', 'r', encoding='utf-8').read())
+	elif chon == '22':
+		# Thanh Công
+		exec(open('tools/TienIchFaceBook/ToolDaoMail.py', 'r', encoding='utf-8').read())
+	elif chon == '23':
+		# Thanh Công
+		exec(open('tools/TienIchFaceBook/ThoatTool.py', 'r', encoding='utf-8').read())          
+	else:
+		sys.exit("")
